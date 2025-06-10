f27df83ce4aa8117f8cdc3b56f8a6fb03a8287ac
Sonia García Martínez <garmarsonia@gmail.com>
2025-06-10 18:30:36 +0200

    feat: install tailwind

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CharactersPage() {
   return (
     <div>
-      <h1>Characters</h1>
+      <h1 className="text-2xl font-bold my-4">Characters</h1>
     </div>
   );
 }
